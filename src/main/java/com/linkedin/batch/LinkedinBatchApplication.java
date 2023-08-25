9624af699422afb00bfee7cb342776f991297422
Daniel Suruagy <dsuruagy@gmail.com>
2023-08-25 17:33:12 -0300

    Job Repository configured to store job information on the database. In this case, using a MySQL database configured on localhost.

--- src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
+++ src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -2,12 +2,9 @@ package com.linkedin.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -31,13 +28,11 @@ public class LinkedinBatchApplication {
 
 	@Bean
 	public Step packageItemStep() {
-		return this.stepBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
-			@Override
-			public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+		return this.stepBuilderFactory.get("packageItemStep").tasklet(
+				(stepContribution, chunkContext) -> {
 				System.out.println("The item has been packaged.");
 				return RepeatStatus.FINISHED;
-			}
-		}).build();
+			}).build();
 	}
 
 	public static void main(String[] args) {
