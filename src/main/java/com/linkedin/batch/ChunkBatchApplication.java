17843519a6c1b77227463abdd3ba10cab801179d
Daniel Suruagy <dsuruagy@gmail.com>
2023-08-29 18:28:05 -0300

    Reading from databases in single-thread scenarios.

--- src/main/java/com/linkedin/batch/ChunkBatchApplication.java
+++ src/main/java/com/linkedin/batch/ChunkBatchApplication.java
@@ -6,25 +6,29 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.FileSystemResource;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class ChunkBatchApplication {
     public static String[] tokens = new String[] {"order_id", "first_name", "last_name", "email", "cost", "item_id", "item_name", "ship_date"};
 
+    public static String ORDER_SQL = "select order_id, first_name, last_name, "
+            + "email, cost, item_id, item_name, ship_date "
+            + "from SHIPPED_ORDER order by order_id";
+
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
 
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public javax.sql.DataSource datasource;
 
     @Bean
     public Job job() {
@@ -35,20 +39,11 @@ public class ChunkBatchApplication {
 
     @Bean
     public ItemReader<Order> itemReader() {
-        FlatFileItemReader<Order> itemReader = new FlatFileItemReader<Order>();
-        itemReader.setLinesToSkip(1);
-        itemReader.setResource(new FileSystemResource("C://Users//CYFS//dev//projetos//linkedin-batch//shipped_orders.csv"));
-
-        DefaultLineMapper<Order> lineMapper = new DefaultLineMapper<Order>();
-        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
-        tokenizer.setNames(tokens);
-
-        lineMapper.setLineTokenizer(tokenizer);
-
-        lineMapper.setFieldSetMapper(new OrderFieldSetMapper());
-
-        itemReader.setLineMapper(lineMapper);
-        return itemReader;
+        return new JdbcCursorItemReaderBuilder<Order>().dataSource(datasource)
+                .name("jdbcCursorItemReader")
+                .sql(ORDER_SQL)
+                .rowMapper(new OrderRowMapper())
+                .build();
     }
 
     @Bean
